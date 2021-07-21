f4751e621d535a7676ac290ac60572254c3cbc16
Rufian55 <chriskearns@rigidbody.tech>
2021-07-21 14:58:46 -0500

    Refactored to new dBase schema

--- AddNewAsset.php
+++ AddNewAsset.php
@@ -1,28 +1,28 @@
 <?php
-//Turn on error reporting.
+//Turn on error reporting
 ini_set('display_errors', 'On');
-//Connect to the database.
-$mysqli = new mysqli("localhost", "user", "password", "database");
+// Import dBase Credentials.
+require('../../project/g3f2Kcd57nE4s25.php');
+// Connect to the database.
+$mysqli = new mysqli($servername, $username, $password, $database);
 ?>
+
 <!DOCTYPE HTML>
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta http-equiv="cache-control" content="no-cache" />
-<link rel="stylesheet" type="text/css" href="../Diamonds/style.css" />
-<link rel="shortcut icon" href="../Diamonds/images/favicon.ico" />
-<title>Diamond Buyers Inc.</title>
-<script src="datePrep.js"></script>
-</head>
+	<head>
+		<?php
+			include('../includes/HeadMB.php');
+		?>
+	</head>
+	<body>
+		<h1>Diamond Buyers Inc.</h1>
+		<h2>Add New Asset</h2>
+		<?php // Get 'to be Owned_By' Account ID.
+			if(isset($_POST['AddNewAsset'])){
+				$id = $_POST['AddNewAsset'];
+			}
+		?>
 
-<body>
-<h1>Diamond Buyers Inc.</h1>
-<h2>Add New Asset</h2>
-<?php // Get to be Owned_By Account ID.
-if(isset($_POST['AddNewAsset'])){
-	$id = $_POST['AddNewAsset'];
-}
-?>
 <div>
 	<fieldset class="fieldset-auto-width">
 	<legend>Data Entry</legend>
@@ -30,24 +30,24 @@ if(isset($_POST['AddNewAsset'])){
 			<form method="post">
             	<table class="ANC">
 				<tr>
-				<td width="100">Description:</td>
-                <td width="200"><input name="Descr" type="text" id="Descr" maxlength="255" size="30" autocomplete="off" required></td>
+				<td width="100">Name:</td>
+                <td width="200"><input name="Name" type="text" id="Name" maxlength="25" size="30" autocomplete="off" required></td>
 				</tr>
 				<tr>
-                <td>Carrot:</td>
-                <td><input name="Carrot" type="text" id="Carrot" maxlength="12" size="12" autocomplete="off" required></td>
+                <td>Description:</td>
+                <td><input name="Descr" type="text" id="Descr" maxlength="255" size="30" autocomplete="off" required></td>
 				</tr>
                 <tr>
-                <td>Cut:
-                <td><input name="Cut" type="text" id="Cut" maxlength="10" size="10" autocomplete="off" required></td>
+                <td>Radius:</td>
+                <td><input name="Radius" type="text" id="Radius" maxlength="11" size="13" autocomplete="off" required></td>
                 </tr>
                 <tr>
-				<td>Clarity:</td>
-                <td><input name="Clarity" type="text" id="Clarity" maxlength="5" size="5" autocomplete="off" required></td>
+				<td>Mass:</td>
+                <td><input name="Mass" type="text" id="Mass" maxlength="11" size="13" autocomplete="off" required></td>
 				</tr>
                 <tr>
-                <td>Color:</td>
-                <td><input name="Color" type="text" id="Color" maxlength="1" size="2" autocomplete="off" required></td>
+                <td>Apparent Magnitude:</td>
+                <td><input name="ApMag" type="text" id="ApMag" maxlength="7" size="10" autocomplete="off" required></td>
 				</tr>
 				<tr>
                 <td>Create Date:</td>
@@ -65,34 +65,34 @@ if(isset($_POST['AddNewAsset'])){
 		</fieldset>
 	</fieldset>
 	<br><br>
-	<button type="button" class="button" onclick="location.href = 'http://web.engr.oregonstate.edu/~kearnsc/Diamonds/';">Return to Main Page</button>
+	<button type="button" class="button" onclick="location.href = 'IndexMB.php';">Return to Main Page</button>
 	<br><br>
 </div>
 
-<?php
-/* Form handler - Executes on 'Add Asset' submit button clicked. */
-if(isset($_POST['submit'])){
-	/* Prepare statement for INSERT new customer's details. */
-	if(!($stmt = $mysqli->prepare("INSERT INTO Asset (Description, Carrot, Cut, Clarity, Color, Create_Date, Owned_By)
-								   VALUES(?,?,?,?,?,?,?)"))) {
-			echo "<p class=\"error\">Prepare for Asset INSERT query failed: "  . $stmt->errno . " " . $stmt->error . "</p>" ; 
-	}
+		<?php
+			/* Form handler - Executes on 'Add Asset' submit button clicked. */
+			if(isset($_POST['submit'])){
+				/* Prepare statement for INSERT new customer's details. */
+				if(!($stmt = $mysqli->prepare("INSERT INTO Asset (Name, Description, Radius, Mass, ApMag, Create_Date, Owned_By)
+											   VALUES(?,?,?,?,?,?,?)"))) {
+						echo "<p class=\"error\">Prepare for Asset INSERT query failed: "  . $stmt->errno . " " . $stmt->error . "</p>" ; 
+				}
 
-	/* Bind Parameters for INSERT new Asset's details. */
-	if(!($stmt->bind_param("sdssssi", $_POST['Descr'], $_POST['Carrot'], $_POST['Cut'], $_POST['Clarity'], $_POST['Color'], $_POST['cDate'], $_POST['Owned_By']))){
-	echo "<p class=\"error\">Bind failed: "  . $stmt->errno . " " . $stmt->error . "</p>";
-	}
+				/* Bind Parameters for INSERT new Asset's details. */
+				if(!($stmt->bind_param("sdssssi", $_POST['Name'], $_POST['Descr'], $_POST['Radius'], $_POST['Mass'], $_POST['ApMag'], $_POST['cDate'], $_POST['Owned_By']))) {
+					echo "<p class=\"error\">Bind failed: "  . $stmt->errno . " " . $stmt->error . "</p>";
+				}
 
-	/* Execute INSERT new Asset's details. */
-	if(!$stmt->execute()){
-		echo "<p class=\"error\">Execute failed: "  . $stmt->errno . " " . $stmt->error . "</p>";
-	} else {
-		echo "<p class=\"success\">Added " . $stmt->affected_rows . " new Asset to Asset table.</p>";
-	}
+				/* Execute INSERT new Asset's details. */
+				if(!$stmt->execute()){
+					echo "<p class=\"error\">Execute failed: "  . $stmt->errno . " " . $stmt->error . "</p>";
+				} else {
+					echo "<p class=\"success\">Added " . $stmt->affected_rows . " new Asset to Asset table.</p>";
+				}
 
-	$stmt->close();
-}
-?>
+				$stmt->close();
+			}
+		?>
 
-</body>
+	</body>
 </html>
