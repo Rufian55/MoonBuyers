82f61942de7bd36dec56e90c582e013936a17049
Rufian55 <chriskearns@rigidbody.tech>
2021-07-21 14:57:57 -0500

    includes Head, favicon

---
+++ includes/HeadMB.php
@@ -0,0 +1,6 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="cache-control" content="no-cache" />
+<link rel="stylesheet" type="text/css" href="../css/style.css" />
+<link rel="shortcut icon" href="../../img/MBfavicon.ico" />
+<title>MoonBuyers</title>
+<script src="datePrep.js"></script>
