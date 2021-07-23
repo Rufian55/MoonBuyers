89f73c3780f20d29bc793ff90acb8ddc092eb453
Rufian55 <chriskearns@rigidbody.tech>
2021-07-23 11:50:17 -0500

    Added bootstrap sanity with some jQuery

--- AddNewCustomer.php
+++ AddNewCustomer.php
@@ -1,10 +1,10 @@
 <?php
-//Turn on error reporting
-ini_set('display_errors', 'On');
-// Import dBase Credentials.
-require('../../project/g3f2Kcd57nE4s25.php');
-// Connect to the database.
-$mysqli = new mysqli($servername, $username, $password, $database);
+	//Turn on error reporting
+	ini_set('display_errors', 'On');
+	// Import dBase Credentials.
+	require('../../project/g3f2Kcd57nE4s25.php');
+	// Connect to the database.
+	$mysqli = new mysqli($servername, $username, $password, $database);
 ?>
 
 <!DOCTYPE HTML>
@@ -17,122 +17,103 @@ $mysqli = new mysqli($servername, $username, $password, $database);
 </head>
 
 <body>
-<h1>Diamond Buyers Inc.</h1>
-<h2>Add New Customer</h2>
-<div>
-	<fieldset class="fieldset-auto-width">
-	<legend>Data Entry</legend>
-		<fieldset class="fieldset-left">
+	<h1>MoonBuyers InterGalactic</h1>
+	<h2>Add New Customer</h2>
+	<div>
+		<div>
+			<h3>Data Entry</h3>
 			<form method="post">
-            	<table class="ANC">
-				<tr>
-				<td width="148">Last Name:</td>
-                <td width="209"><input name="Lname" type="text" id="Lname" maxlength="30" size="30" autocomplete="off" required></td>
-				</tr>
-				<tr>
-                <td>First Name:</td>
-                <td><input name="Fname" type="text" id="Fname" maxlength="30" size="30" autocomplete="off" required></td>
-				</tr>
-                <tr>
-                <td>Address 1:</td>
-                <td><input name="Addr_1" type="text" id="Addr_1" maxlength="30" size="30" autocomplete="off" required></td>
-                </tr>
-                <tr>
-				<td>Address 2:</td>
-                <td><input name="Addr_2" type="text" id="Addr_2" maxlength="30" size="30" autocomplete="off" required></td>
-				</tr>
-                <tr>
-                <td>City:</td>
-                <td><input name="City" type="text" id="City" maxlength="25" size="25" autocomplete="off" required></td>
-				</tr>
-				<tr>
-                <td>State:</td>
-                <td><input name="State" type="text" id="State" maxlength="2" size="2" autocomplete="off" required></td>
-				</tr>
-				<tr>
-                <td>Planet:</td>
-                <td><input name="Planet" type="text" id="Planet" maxlength="25" size="25" autocomplete="off" required></td>
-				</tr>
-                <tr>
-				<td>Zip:</td>
-                <td><input name="Zip" type="text" id="Zip" maxlength="15" size="18" autocomplete="off" required></td>
-                </tr>
-                <tr>
-				<td>Phone:</td>
-                <td><input name="Phone" type="text" id="Phone" maxlength="20" size="20" autocomplete="off" required></td>
-                </tr>
-				<tr>
-				<td>Opening Balance:</td>
-                <td><input name="Open" type="text" id="Open" maxlength="10" size="10" autocomplete="off" required value="0.00"></td>
-                </tr>
-				<tr><td></td><td align="right"><input type="submit" value="Add Customer" name="submit" id="submit"></td></tr>
-                </table>
+				<div class="form-group container">
+					<input class="form-control" type="text" maxlength="30" name="Lname" id="Lname" placeholder="Customer Oganized As: LLC, Ltd. etc." required>
+					<input class="form-control" type="text" maxlength="30" name="Fname" id="Fname" placeholder="Business Name" required>
+					<input class="form-control" type="text" maxlength="30" name="Addr_1" id="Addr_1" placeholder="Address 1" required>
+					<input class="form-control" type="text" maxlength="30" name="Addr_2" id="Addr_2" placeholder="Address 2" required>
+					<input class="form-control" type="text" maxlength="25" name="City" id="City" placeholder="City" required>
+					<input class="form-control" type="text" maxlength="2" name="State" id="State" placeholder="State" required>
+					<input class="form-control" type="text" maxlength="25" name="Planet" id="Planet" placeholder="Planet" required>
+					<input class="form-control" type="number" maxlength="20" min="0" name="Zip" id="Zip" placeholder="Zip" required>
+					<input class="form-control" type="number" maxlength="20" min="0" name="Phone" id="Phone" placeholder="Phone" required>
+					<input class="form-control" type="number" min="0" step="0.01" name="Open" id="Open" placeholder="Opening Balance" required>
+					<br><br>
+					<input type="submit" type="reset" value="Add Customer" name="submit" id="submit">
+				</div>
 			</form>
-		</fieldset>
-	</fieldset>
-	<br><br>
-	<button type="button" class="button" onclick="location.href='IndexMB.php';">Return to Main Page</button>
-	<br><br>
-</div>
+		</div>
+		<br><br>
+		<button type="button" class="button" onclick="location.href='IndexMB.php';">Return to Main Page</button>
+		<br><br>
+	</div>
 
-<?php
-/* Form handler - Executes on 'Add Customer' submit button clicked. */
-if(isset($_POST['submit'])){
-	/* Lock tables for writing - protect integrity of new custoemr's ID retrieval. */
-	if(!$mysqli->query("LOCK TABLES Customers WRITE, Account WRITE")) {
-		echo "<p class=\"error\">ERROR! Tables did not lock for writing: (" . $mysqli->errno . ")" . $mysqli->error . "</p>";
-	}
+	<?php
+		/* Form handler - Executes on 'Add Customer' submit button clicked. */
+		if(isset($_POST['submit'])){
+			/* Lock tables for writing - protect integrity of new custoemr's ID retrieval. */
+			if(!$mysqli->query("LOCK TABLES Customers WRITE, Account WRITE")) {
+				echo "<p class=\"error\">ERROR! Tables did not lock for writing: (" . $mysqli->errno . ")" . $mysqli->error . "</p>";
+			}
 
-	/* Prepare statement for INSERT new customer's details. */
-	if(!($stmt = $mysqli->prepare("INSERT INTO Customers (Lname, Fname, Addr_1, Addr_2, City, State, Planet, Zip, Phone)
-								   VALUES(?,?,?,?,?,?,?,?,?)"))) {
-			echo "<p class=\"error\">Prepare for Customers INSERT query failed: "  . $stmt->errno . " " . $stmt->error . "</p>" ; 
-	}
+			/* Prepare statement for INSERT new customer's details. */
+			if(!($stmt = $mysqli->prepare("INSERT INTO Customers (Lname, Fname, Addr_1, Addr_2, City, State, Planet, Zip, Phone)
+										   VALUES(?,?,?,?,?,?,?,?,?)"))) {
+					echo "<p class=\"error\">Prepare for Customers INSERT query failed: "  . $stmt->errno . " " . $stmt->error . "</p>" ; 
+			}
 
-	/* Bind Parameters for INSERT new customer's details. */
-	if(!($stmt->bind_param("sssssssii", $_POST['Fname'], $_POST['Lname'], $_POST['Addr_1'], $_POST['Addr_2'], $_POST['City'], $_POST['State'], $_POST['Planet'], $_POST['Zip'], $_POST['Phone']))){
-		echo "<p class=\"error\">Bind failed: "  . $stmt->errno . " " . $stmt->error . "</p>";
-	}
+			/* Bind Parameters for INSERT new customer's details. */
+			if(!($stmt->bind_param("sssssssii", $_POST['Fname'], $_POST['Lname'], $_POST['Addr_1'], $_POST['Addr_2'], $_POST['City'], $_POST['State'], $_POST['Planet'], $_POST['Zip'], $_POST['Phone']))){
+				echo "<p class=\"error\">Bind failed: "  . $stmt->errno . " " . $stmt->error . "</p>";
+			}
 
-	/* Execute INSERT new customer's details. */
-	if(!$stmt->execute()){
-		echo "<p class=\"error\">Execute failed: "  . $stmt->errno . " " . $stmt->error . "</p>";
-	} else {
-		echo "<p class=\"success\">Added " . $stmt->affected_rows . " new Customer to table \"Customers\".</p>";
-	}
+			/* Execute INSERT new customer's details. */
+			if(!$stmt->execute()){
+				echo "<p class=\"error\">Execute failed: "  . $stmt->errno . " " . $stmt->error . "</p>";
+			} else {
+				echo "<p class=\"success\">Added " . $stmt->affected_rows . " new Customer to table \"Customers\".</p>";
+			}
 
-	/* Retrieve last insert ID which = new Customer ID.
-	   Adapted from http://www.w3schools.com/php/php_mysql_insert_lastid.asp */
-	if($temp = $mysqli->insert_id) {
-		echo "<p class=\"success\">New Cusotmer ID = " . $temp . "</p>";
-	} else {
-    	echo "<p class=\"error\">There was an error retrieving the new Customer ID. Error: " . $mysqli->error . "</p>";
-	}
+			/* Retrieve last insert ID which = new Customer ID. */
+			if($temp = $mysqli->insert_id) {
+				echo "<p class=\"success\">New Customer ID = " . $temp . "</p>";
+			} else {
+		    	echo "<p class=\"error\">There was an error retrieving the new Customer ID. Error: " . $mysqli->error . "</p>";
+			}
 
-	/* Prepare statement for new customer's new account. */
-	if(!($stmt = $mysqli->prepare("INSERT INTO Account (C_ID, Balance) VALUES(?,?)"))) {
-		echo "<p classs=\"error\">Prepare for Account INSERT query failed: "  . $stmt->errno . " " . $stmt->error . "</p>";
-	}
+			/* Prepare statement for new customer's new account. */
+			if(!($stmt = $mysqli->prepare("INSERT INTO Account (C_ID, Balance) VALUES(?,?)"))) {
+				echo "<p classs=\"error\">Prepare for Account INSERT query failed: "  . $stmt->errno . " " . $stmt->error . "</p>";
+			}
 
-	/* Bind Parameters for INSERT new customer's Account details. */
-	if(!($stmt->bind_param("ii", $temp, $_POST['Open']))){
-		echo "<p class=\"error\">Bind failed: "  . $stmt->errno . " " . $stmt->error . "</p>";
-	}
+			/* Bind Parameters for INSERT new customer's Account details. */
+			if(!($stmt->bind_param("ii", $temp, $_POST['Open']))){
+				echo "<p class=\"error\">Bind failed: "  . $stmt->errno . " " . $stmt->error . "</p>";
+			}
 
-	/* Execute INSERT new customer's Account details. */
-	if(!$stmt->execute()){
-		echo "<p class=\"error\">Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error . "</p>";
-	}
+			/* Execute INSERT new customer's Account details. */
+			if(!$stmt->execute()){
+				echo "<p class=\"error\">Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error . "</p>";
+			}
 
-	/* UNLOCK tables. */
-	if(!$mysqli->query("UNLOCK TABLES")) {
-		echo "<p class=\"error\">ERROR! Tables did not unlock following write: (" . $mysqli->errno . ")" . $mysqli->error . "</p>";
-	}
+			/* UNLOCK tables. */
+			if(!$mysqli->query("UNLOCK TABLES")) {
+				echo "<p class=\"error\">ERROR! Tables did not unlock following write: (" . $mysqli->errno . ")" . $mysqli->error . "</p>";
+			}
 
-	$stmt->close();
-}
-?>
+			$stmt->close();
+		}
+	?>
+	
+	<!-- Only two decimal places please! -->
+	<script
+	 src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous">
+	</script>
+
+	<script>
+		$("#Open").blur(function() {
+    	this.value = parseFloat(this.value).toFixed(2);
+		});
+		$("#Zip, #Phone").blur(function() {
+			this.value = parseFloat(this.value).toFixed(0);
+		});
+	</script>
 
 </body>
-
 </html>
