825bc0da36b7e83600f7006d556064f32a2cfc4e
Rufian55 <chriskearns@rigidbody.tech>
2021-08-03 05:52:29 -0500

    Bug fix: AddAsset account empty...

--- IndexMB.php
+++ IndexMB.php
@@ -29,13 +29,8 @@
 	
 	<div class="row">
 		<div class="col-md-2"></div>
-        <div class="col-md-2">
-	       	<div class="scroll-container col-centered">
-  				<div class="chevron"></div>
-  				<div class="chevron"></div>
-  				<div class="chevron"></div>
-  			</div>
-        </div>
+        <div class="col-md-2"></div>
+        <div class="col-md-2"></div>
         <div class="col-md-2"></div>
         <div class="col-md-2"></div>
         <div class="col-md-2">
@@ -45,7 +40,7 @@
         		<div class="chevron"></div>
         	</div>
         </div>
-		<div class="col-md-2"></div>
+		
     </div>
 	
 	<div>
